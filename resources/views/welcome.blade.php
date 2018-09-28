1a60599325473685a6b8e5aaac479e800bf070dd
ya310 <mentol310@gmail.com>
2018-09-28 23:58:35 +0900

    なんか welcome ページをそれっぽく↓:wq

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Vuetify Parallax Starter</title>
+    <title>在庫管理 -Zaikok-</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
@@ -23,15 +23,15 @@
                             class="white--text"
                     >
                         <img src="{{ asset('images/vuetify.png') }}" alt="Vuetify.js" height="200">
-                        <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
-                        <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
+                        <h1 class="white--text mb-2 display-1 text-xs-center">在庫管理 -zaikok-</h1>
+                        <div class="subheading mb-3 text-xs-center">Powered by ya-sato@mentol</div>
                         <v-btn
                                 class="blue lighten-2 mt-5"
                                 dark
                                 large
-                                href="/pre-made-themes"
+                                href="/register"
                         >
-                            Get Started
+                            はじめる
                         </v-btn>
                     </v-layout>
                 </v-parallax>
@@ -46,10 +46,10 @@
                 >
                     <v-flex xs12 sm4 class="my-3">
                         <div class="text-xs-center">
-                            <h2 class="headline">The best way to start developing</h2>
+                            <h2 class="headline">在庫を管理する最善の方法</h2>
                             <span class="subheading">
-                Cras facilisis mi vitae nunc
-              </span>
+                                あなたの手で在庫管理が加速します
+                            </span>
                         </div>
                     </v-flex>
                     <v-flex xs12>
@@ -61,12 +61,13 @@
                                             <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
                                         </v-card-text>
                                         <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline text-xs-center">Material Design</div>
+                                            <div class="headline text-xs-center">スピーディな在庫管理</div>
                                         </v-card-title>
                                         <v-card-text>
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                                            すぐにオンラインで在庫管理ができるZaikok。
+                                            難しい操作は必要ありません。
+                                            最新のWeb技術の活用で驚くほど簡単に、
+                                            時間とコストをかけることなく在庫管理を行えます。
                                         </v-card-text>
                                     </v-card>
                                 </v-flex>
@@ -76,12 +77,11 @@
                                             <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
                                         </v-card-text>
                                         <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline">Fast development</div>
+                                            <div class="headline">リーズナブルな費用</div>
                                         </v-card-title>
                                         <v-card-text>
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                                            サービス利用はフリーです。
+                                            全てフリー、責任もフリーです。
                                         </v-card-text>
                                     </v-card>
                                 </v-flex>
@@ -91,12 +91,12 @@
                                             <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
                                         </v-card-text>
                                         <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline text-xs-center">Completely Open Sourced</div>
+                                            <div class="headline text-xs-center">オープンソースでいつでも要望、開発も</div>
                                         </v-card-title>
                                         <v-card-text>
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                                            全てはオープンソースに始まります。
+                                            経験欠乏な担当者のレポジトリにアクセスし要望を送りましょう。
+                                            答えるかはわかりません。
                                         </v-card-text>
                                     </v-card>
                                 </v-flex>
@@ -109,15 +109,15 @@
             <section>
                 <v-parallax src="{{ asset('images/section.jpg') }}" height="380">
                     <v-layout column align-center justify-center>
-                        <div class="headline white--text mb-3 text-xs-center">Web development has never been easier</div>
-                        <em>Kick-start your application today</em>
+                        <div class="headline white--text mb-3 text-xs-center">在庫管理ってこんな簡単だったんだ</div>
+                        <em>今日からWebで在庫管理を始められます</em>
                         <v-btn
                                 class="blue lighten-2 mt-5"
                                 dark
                                 large
-                                href="/pre-made-themes"
+                                href="/register"
                         >
-                            Get Started
+                            はじめる
                         </v-btn>
                     </v-layout>
                 </v-parallax>
@@ -129,22 +129,20 @@
                         <v-flex xs12 sm4>
                             <v-card class="elevation-0 transparent">
                                 <v-card-title primary-title class="layout justify-center">
-                                    <div class="headline">Company info</div>
+                                    <div class="headline">作った人情報</div>
                                 </v-card-title>
                                 <v-card-text>
-                                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                                    適当な日々をのんのんと生きてるプログラマーです。
                                 </v-card-text>
                             </v-card>
                         </v-flex>
                         <v-flex xs12 sm4 offset-sm1>
                             <v-card class="elevation-0 transparent">
                                 <v-card-title primary-title class="layout justify-center">
-                                    <div class="headline">Contact us</div>
+                                    <div class="headline">コンタクトを取る</div>
                                 </v-card-title>
                                 <v-card-text>
-                                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                                    もし興味を持ったなら続きは web で
                                 </v-card-text>
                                 <v-list class="transparent">
                                     <v-list-tile>
@@ -152,7 +150,7 @@
                                             <v-icon class="blue--text text--lighten-2">phone</v-icon>
                                         </v-list-tile-action>
                                         <v-list-tile-content>
-                                            <v-list-tile-title>777-867-5309</v-list-tile-title>
+                                            <v-list-tile-title>012-345-6789</v-list-tile-title>
                                         </v-list-tile-content>
                                     </v-list-tile>
                                     <v-list-tile>
@@ -160,7 +158,7 @@
                                             <v-icon class="blue--text text--lighten-2">place</v-icon>
                                         </v-list-tile-action>
                                         <v-list-tile-content>
-                                            <v-list-tile-title>Chicago, US</v-list-tile-title>
+                                            <v-list-tile-title>Japan, JP</v-list-tile-title>
                                         </v-list-tile-content>
                                     </v-list-tile>
                                     <v-list-tile>
@@ -168,7 +166,7 @@
                                             <v-icon class="blue--text text--lighten-2">email</v-icon>
                                         </v-list-tile-action>
                                         <v-list-tile-content>
-                                            <v-list-tile-title>john@vuetifyjs.com</v-list-tile-title>
+                                            <v-list-tile-title>mentol310@gmail.com</v-list-tile-title>
                                         </v-list-tile-content>
                                     </v-list-tile>
                                 </v-list>
@@ -184,8 +182,7 @@
                         <div class="white--text ml-3">
                             Made with
                             <v-icon class="red--text">favorite</v-icon>
-                            by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-                            and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+                            by <a class="white--text" href="https://github.com/mentol0126" target="_blank">ya-sato@mentl</a>
                         </div>
                     </v-flex>
                 </v-layout>
@@ -200,7 +197,7 @@
     el: '#app',
     data () {
       return {
-        title: 'Your Logo'
+        title: '在庫管理 -Zaikok-'
       }
     }
   })
