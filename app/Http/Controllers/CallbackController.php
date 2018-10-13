4c7be02858de05263dda0b69a3f38e3d02e48acc
ya310 <mentol310@gmail.com>
2018-10-13 14:09:25 +0900

    サンプルを使ってみる

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -3,33 +3,40 @@
 namespace Zaikok\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
+use Illuminate\Log\Logger;
 use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\CarouselContainerBuilder;
+use LINE\LINEBot\KitchenSink\EventHandler\MessageHandler;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\KitchenSink\EventHandler\MessageHandler\TextMessageHandler;
 
 class CallbackController extends Controller
 {
     /**
      * @param Request $request
-     * @throws \Throwable
+     * @param Logger $logger
      */
-    public function index(Request $request)
+    public function index(Request $request, Logger $logger)
     {
         $httpClient = new CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
         $bot        = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
 
-        $signature = $request->header(LINEBot\Constant\HTTPHeader::LINE_SIGNATURE);
+        $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
         $events    = $bot->parseEventRequest($request->getContent(), $signature);
 
-        /** @var LINEBot\Event\MessageEvent\TextMessage $event */
         foreach ($events as $event) {
-            $msg = $event->getText();
-
-            if ('hoge' === $msg) {
-                $bot->replyText($event->getReplyToken(), 'ok');
-            } else {
-                $bot->replyText($event->getReplyToken(), 'ng');
+            if ($event instanceof MessageEvent) {
+                if ($event instanceof TextMessage) {
+                    $handler = new TextMessageHandler($bot, $logger, $request, $event);
+                }
             }
         }
+
+        $handler->handle();
     }
 }
