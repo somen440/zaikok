c9ff38087be8585dc8ecdb9a7a42ddbeababcf9d
ya310 <mentol310@gmail.com>
2018-10-14 13:01:29 +0900

    シードに生成日時追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,9 +14,11 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name'     => '管理者まん',
-            'email'    => 'admin',
-            'password' => bcrypt('secret'),
+            'name'       => '管理者まん',
+            'email'      => 'admin',
+            'password'   => bcrypt('secret'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
