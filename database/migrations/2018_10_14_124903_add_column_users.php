4dfc110462a06313dc97015309112b5070eb2c6e
ya310 <mentol310@gmail.com>
2018-10-14 13:02:07 +0900

    users テーブルにライン認証用カラム追加

---
+++ database/migrations/2018_10_14_124903_add_column_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedSmallInteger('line_verify_token')->after('line_id')->nullable();
+            $table->index('line_verify_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('line_verify_token');
+        });
+    }
+}
