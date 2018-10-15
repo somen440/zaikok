e3eb43c324bbc2ef0f2f5ad3e44a93d004ad3a1c
ya310 <mentol310@gmail.com>
2018-10-15 23:20:31 +0900

    画像を保存できるように

--- app/Handler/ImegeMessageHandler.php
+++ app/Handler/ImegeMessageHandler.php
@@ -2,18 +2,21 @@
 
 namespace Zaikok\Handler;
 
-use LINE\LINEBot;
+use Illuminate\Support\Facades\Storage;
 use LINE\LINEBot\Event\MessageEvent\ImageMessage;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 
 class ImegeMessageHandler extends AbstractHandler
 {
-    public static function create(LINEBot $bot, ImageMessage $imageMessage): self
+    public static function create($bot, ImageMessage $imageMessage): self
     {
         $messages = [];
         $contentId = $imageMessage->getMessageId();
         $image = $bot->getMessageContent($contentId)->getRawBody();
-        $messages[] = new TextMessageBuilder($image);
+
+        Storage::put('file.jpg', $image);
+        $url = Storage::url('file.jpg');
+        $messages[] = new ImageMessageBuilder($url, $url);
 
         return new self($bot, $imageMessage->getReplyToken(), $messages);
     }
