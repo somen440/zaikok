775600441253e37a691513ba40997f47eed210a6
ya310 <mentol310@gmail.com>
2018-10-15 23:42:46 +0900

    base path を足してみる

--- app/Handler/ImageMessageHandler.php
+++ app/Handler/ImageMessageHandler.php
@@ -3,6 +3,7 @@
 namespace Zaikok\Handler;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use LINE\LINEBot;
 use LINE\LINEBot\Event\MessageEvent\ImageMessage;
@@ -18,7 +19,7 @@ class ImageMessageHandler extends AbstractHandler
         $image = $bot->getMessageContent($contentId)->getRawBody();
 
         Storage::put('public/file.jpg', $image);
-        $url = Storage::url('public/file.jpg');
+        $url = base_path() . Storage::url('public/file.jpg');
         $messages[] = new TextMessageBuilder($url);
         $messages[] = new ImageMessageBuilder($url, $url);
 
